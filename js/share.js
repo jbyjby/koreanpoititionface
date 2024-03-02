c0aa10e2f550cb38cd0b752d33449ed358bf2899
jbyjby <jby8775@gmail.com>
2024-03-03 04:47:29 +0900

    평균사진추가

--- js/share.js
+++ js/share.js
@@ -1,11 +1,126 @@
-function shareMessage() {
+function shareMessage0() {
   console.log("공유된다.");
   Kakao.Share.sendDefault({
     objectType: "feed",
     content: {
       title: `관상으로 보는 정치성향 테스트🥰내가 진보의 상인가? 보수의 상인가?`,
       description: "#관상테스트 #410총선 #진보 #보수 #투표소찾기 #우리동네현안보기",
-      imageUrl: "http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      imageUrl: "https://github.com/jbyjby/koreanpoititionface/blob/main/img/bg1.png?raw=true",
+
+      link: {
+        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+        mobileWebUrl: "http://127.0.0.1:5500",
+        webUrl: "http://127.0.0.1:5500",
+      },
+    },
+    social: {
+      likeCount: 286,
+      commentCount: 45,
+      sharedCount: 845,
+    },
+    buttons: [
+      {
+        title: "나도 해보기",
+        link: {
+          mobileWebUrl: "http://127.0.0.1:5500",
+          webUrl: "hhttp://127.0.0.1:5500",
+        },
+      },
+      {
+        title: "투표소찾기",
+        link: {
+          mobileWebUrl: "https://nec.go.kr",
+          webUrl: "https://nec.go.kr",
+        },
+      },
+    ],
+  });
+}
+
+function shareMessage1() {
+  console.log("공유된다.");
+  Kakao.Share.sendDefault({
+    objectType: "feed",
+    content: {
+      title: `관상으로 보는 정치성향 테스트🥰내가 진보의 상인가? 보수의 상인가?`,
+      description: "#관상테스트 #410총선 #진보 #보수 #투표소찾기 #우리동네현안보기",
+      imageUrl: "../img/left1.jpg",
+      link: {
+        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+        mobileWebUrl: "http://127.0.0.1:5500",
+        webUrl: "http://127.0.0.1:5500",
+      },
+    },
+    social: {
+      likeCount: 286,
+      commentCount: 45,
+      sharedCount: 845,
+    },
+    buttons: [
+      {
+        title: "나도 해보기",
+        link: {
+          mobileWebUrl: "http://127.0.0.1:5500",
+          webUrl: "hhttp://127.0.0.1:5500",
+        },
+      },
+      {
+        title: "투표소찾기",
+        link: {
+          mobileWebUrl: "https://nec.go.kr",
+          webUrl: "https://nec.go.kr",
+        },
+      },
+    ],
+  });
+}
+
+function shareMessage2() {
+  console.log("공유된다.");
+  Kakao.Share.sendDefault({
+    objectType: "feed",
+    content: {
+      title: `관상으로 보는 정치성향 테스트🥰내가 진보의 상인가? 보수의 상인가?`,
+      description: "#관상테스트 #410총선 #진보 #보수 #투표소찾기 #우리동네현안보기",
+      imageUrl: "../img/right2.jpg",
+      link: {
+        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+        mobileWebUrl: "http://127.0.0.1:5500",
+        webUrl: "http://127.0.0.1:5500",
+      },
+    },
+    social: {
+      likeCount: 286,
+      commentCount: 45,
+      sharedCount: 845,
+    },
+    buttons: [
+      {
+        title: "나도 해보기",
+        link: {
+          mobileWebUrl: "http://127.0.0.1:5500",
+          webUrl: "hhttp://127.0.0.1:5500",
+        },
+      },
+      {
+        title: "투표소찾기",
+        link: {
+          mobileWebUrl: "https://nec.go.kr",
+          webUrl: "https://nec.go.kr",
+        },
+      },
+    ],
+  });
+}
+
+function shareMessage3() {
+  console.log("공유된다.");
+  Kakao.Share.sendDefault({
+    objectType: "feed",
+    content: {
+      title: `관상으로 보는 정치성향 테스트🥰내가 진보의 상인가? 보수의 상인가?`,
+      description: "#관상테스트 #410총선 #진보 #보수 #투표소찾기 #우리동네현안보기",
+      imageUrl: "../img/left2.jpg",
       link: {
         // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
         mobileWebUrl: "http://127.0.0.1:5500",
