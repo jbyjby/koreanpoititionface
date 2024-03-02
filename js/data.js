23440561ac1a8d9f23a537bdb27fe700dce354ba
jbyjby <jby8775@gmail.com>
2024-03-03 05:34:13 +0900

    공유보튼 체인지 완료

--- js/data.js
+++ js/data.js
@@ -3,21 +3,25 @@ let data = {
     title: `단정하고 강인한<br><span class="title-red1">보수</span>의 상`,
     description: `단정하고 강인한 정신력을 지닌 당신!<br> 당신은 리더십과 원칙을 중시한다. 굳은 신념과 전통적 가치관을 바탕으로 결정을 내리며, 사회적 질서와 안정을 중요하게 생각한다. 변화보다는 안정을 추구하며, 신중하고 체계적인 접근을 통해 문제를 해결한다.`,
     people: "이승만. 박정희, 김영삼, 홍준표, 이명박, 이준석 등 ",
+    className: "javascript:shareMessage0()",
   },
   보수여: {
     title: `우아하고 단아한<br><span class="title-red1">보수</span>의 상`,
     description: `우아함과 단아함을 겸비한 당신!<br> 당신은 사려 깊고 원칙적이다. 전통적 가치를 중요시하며, 가정과 사회에서의 안정과 조화를 추구합니다. 깊은 사고와 세심한 배려를 바탕으로 주변 사람들과의 관계를 소중히 여긴다.`,
     people: "박근혜, 나경원, 배현진, 이인선 등 ",
+    className: "javascript:shareMessage2()",
   },
   진보남: {
     title: `독립적이고 자유로운<br><span class="title-blue1">진보</span>의 상`,
     description: `독립적이고 자유로운 영혼의 당신!<br>당신은 혁신과 변화를 추구한다. 개방적인 사고방식과 창의적인 해결책으로 새로운 가능성을 탐색하며, 사회적 평등과 다양성을 중시한다. 독창적인 접근 방식으로 사회의 발전과 진보를 위한 새로운 길을 제시한다.`,
     people: "김대중, 노무현, 이재명, 정청래, 박주민, 정봉주 등 ",
+    className: "javascript:shareMessage1()",
   },
   진보여: {
     title: `자유롭고 창의적인<br><span class="title-blue1">진보</span>의 상`,
     description: `자유롭고 창의적인 정신을 지닌 당신!<br>당신은 개방성과 혁신을 추구한다. 평등과 다양성을 사회적 가치로 중요시하며, 개인의 권리와 자유를 적극적으로 옹호한다. 독창적인 길을 모색하며, 사회적 변화를 이끄는 데 앞장선다.`,
     people: "추미애, 심상정, 박영선, 남인순 등 ",
+    className: "javascript:shareMessage3()",
   },
   몰라: {
     title: `무슨상인지 모르것음`,
